18e62ba1f20cbf8e851905aa0d7ccee6974b9dcf
Michelle Lauer <mflauer@mit.edu>
2018-11-29 15:38:26 -0500

    some capsnet architecture stuff

--- models.py
+++ models.py
@@ -14,11 +14,13 @@ K.set_image_data_format('channels_last')
 
 
 class BaseModel:
-    def __init__(self, name, n_class, image_shape, tensorboard, filename=None):
+    def __init__(self, name, n_class, image_shape, tensorboard, filename=None, **kwargs):
         self.name = name
         self.n_class = n_class
         self.image_shape = image_shape
         self.tensorboard = tensorboard
+
+        self.kwargs = kwargs
 
         self._new_model()
         if filename is not None:
@@ -104,7 +106,7 @@ class Autoencoder(BaseModel):
 
 
 class CapsNet(BaseModel):
-	def _new_model(self): #ADD ROUTINGS param: routings
+	def _new_model(self):
     """
     A Capsule Network on MNIST.
     :param self.image_shape: data shape, 3d, [width, height, channels]
@@ -113,6 +115,8 @@ class CapsNet(BaseModel):
     :return: Two Keras Models, the first one used for training, and the second one for evaluation.
             `eval_model` can also be used for training.
     """
+    routings = kwargs['routings']
+
     x = layers.Input(shape=self.image_shape)
 
     # Layer 1: Just a conventional Conv2D layer
@@ -274,61 +278,4 @@ def load_mnist():
     y_train = to_categorical(y_train.astype('float32'))
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
-
-
-if __name__ == "__main__":
-    import os
-    import argparse
-    from keras.preprocessing.image import ImageDataGenerator
-    from keras import callbacks
-
-    # setting the hyper parameters
-    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
-    parser.add_argument('--epochs', default=50, type=int)
-    parser.add_argument('--batch_size', default=100, type=int)
-    parser.add_argument('--lr', default=0.001, type=float,
-                        help="Initial learning rate")
-    parser.add_argument('--lr_decay', default=0.9, type=float,
-                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
-    parser.add_argument('--lam_recon', default=0.392, type=float,
-                        help="The coefficient for the loss of decoder")
-    parser.add_argument('-r', '--routings', default=3, type=int,
-                        help="Number of iterations used in routing algorithm. should > 0")
-    parser.add_argument('--shift_fraction', default=0.1, type=float,
-                        help="Fraction of pixels to shift at most in each direction.")
-    parser.add_argument('--debug', action='store_true',
-                        help="Save weights by TensorBoard")
-    parser.add_argument('--save_dir', default='./result')
-    parser.add_argument('-t', '--testing', action='store_true',
-                        help="Test the trained model on testing dataset")
-    parser.add_argument('--digit', default=5, type=int,
-                        help="Digit to manipulate")
-    parser.add_argument('-w', '--weights', default=None,
-                        help="The path of the saved weights. Should be specified when testing")
-    args = parser.parse_args()
-    print(args)
-
-    if not os.path.exists(args.save_dir):
-        os.makedirs(args.save_dir)
-
-    # load data
-    (x_train, y_train), (x_test, y_test) = load_mnist()
-
-    # define model
-    model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
-                                                  n_class=len(np.unique(np.argmax(y_train, 1))),
-                                                  routings=args.routings)
-    model.summary()
-
-    # train or test
-    if args.weights is not None:  # init the model weights with provided one
-        model.load_weights(args.weights)
-    if not args.testing:
-        train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
-    else:  # as long as weights are given, will run testing
-        if args.weights is None:
-            print('No weights are provided. Will test using random initialized weights.')
-        manipulate_latent(manipulate_model, (x_test, y_test), args)
-        test(model=eval_model, data=(x_test, y_test), args=args)
-
 
