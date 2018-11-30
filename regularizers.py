8ee9423482f132912d80512bd649e1a51c2b4aa3
Larry Zhang <lzhang124@gmail.com>
2018-11-30 16:12:03 -0500

    integrate regularizers

--- regularizers.py
+++ regularizers.py
@@ -2,16 +2,20 @@ import numpy as np
 from numpy import linalg as LA
 
 def combined_regularizer(regularizers, weights):
+    if len(regularizers) == 0:
+        return None
+
+    assert len(regularizers) == len(weights)
+    
     def reg_fn(M):
-        loss= 0
+        loss = 0
         for i in range(len(regularizers)):
             loss += weights[i] * regularizers[i](M)          
         return loss
-
     return reg_fn
 
 
-def l2PerRow(M):
+def l2_row(M):
     d1, d2, d3, d4 = M.shape
     flatM = np.reshape(d1*d2, d3*d4)
     return np.sum(np.square(LA.norm(flatM, axis=1)))
@@ -23,7 +27,7 @@ def frobenius(M):
     return np.sum(LA.norm(entryPerW, 'fro'))
 
 
-def operatorNorm(M):
+def operator_norm(M):
     d1, d2, d3, d4 = M.shape
     entryPerW = np.reshape(d1*d2, d3, d4)
     # ord 2 is operator norm: https://stackoverflow.com/questions/48275198/how-does-numpy-linalg-norm-ord-2-work
