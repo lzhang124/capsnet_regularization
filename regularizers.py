5d9789eb227e493b0e0c10bf6413650fce6f2c7b
Larry Zhang <lzhang124@gmail.com>
2018-12-08 09:36:58 -0500

    as list

--- regularizers.py
+++ regularizers.py
@@ -20,13 +20,13 @@ def l2(W):
 
 
 def l21(W):
-    d1 = K.shape(W)[-2]
+    d1 = K.shape(W).as_list()[-2]
     all_W_rows = K.reshape(W, (-1, d1))
     return K.sum(tf.norm(all_W_rows, axis=1)) / tf.size(W, out_type=tf.float32)
 
 
 def operator_norm(W):
-    d1, d2 = K.shape(W)[-2:]
+    d1, d2 = K.shape(W).as_list()[-2:]
     all_Ws = K.reshape(W, (-1, d1, d2))
     # ord 2 is operator norm: https://stackoverflow.com/questions/48275198/how-does-numpy-linalg-norm-ord-2-work
     return K.sum(tf.norm(all_Ws, ord=2)) / tf.size(W, out_type=tf.float32)
