e5782f4d5c87f43b1638ab0e8da6cdb4e94aaee9
Larry Zhang <lzhang124@gmail.com>
2018-12-08 09:27:50 -0500

    scaled

--- regularizers.py
+++ regularizers.py
@@ -1,4 +1,5 @@
 from keras import backend as K
+from keras import regularizers
 import tensorflow as tf
 
 
@@ -10,14 +11,22 @@ def weighted_regularizer(regularizer, weight):
     return reg_fn
 
 
+def l1(W):
+    return regularizers.l1(1.)(W) / tf.size(W)
+
+
+def l2(W):
+    return regularizers.l1(2.)(W) / tf.size(W)
+
+
 def l21(W):
-    d1 = list(map(int, W.shape))[-2]
+    d1 = K.shape(W)[-2]
     all_W_rows = K.reshape(W, (-1, d1))
-    return K.sum(tf.norm(all_W_rows, axis=1))
+    return K.sum(tf.norm(all_W_rows, axis=1)) / tf.size(W)
 
 
 def operator_norm(W):
-    d1, d2 = list(map(int, W.shape))[-2:]
+    d1 = K.shape(W)[-2:]
     all_Ws = K.reshape(W, (-1, d1, d2))
     # ord 2 is operator norm: https://stackoverflow.com/questions/48275198/how-does-numpy-linalg-norm-ord-2-work
-    return K.sum(tf.norm(all_Ws, ord=2))
+    return K.sum(tf.norm(all_Ws, ord=2)) / tf.size(W)
