e07b81500c2056569e8d241d40caadf9933a679d
Larry Zhang <lzhang124@gmail.com>
2018-12-08 09:30:57 -0500

    float

--- regularizers.py
+++ regularizers.py
@@ -12,21 +12,21 @@ def weighted_regularizer(regularizer, weight):
 
 
 def l1(W):
-    return regularizers.l1(1.)(W) / tf.size(W)
+    return regularizers.l1(1.)(W) / tf.size(W, out_type=tf.float32)
 
 
 def l2(W):
-    return regularizers.l1(2.)(W) / tf.size(W)
+    return regularizers.l1(2.)(W) / tf.size(W, out_type=tf.float32)
 
 
 def l21(W):
     d1 = K.shape(W)[-2]
     all_W_rows = K.reshape(W, (-1, d1))
-    return K.sum(tf.norm(all_W_rows, axis=1)) / tf.size(W)
+    return K.sum(tf.norm(all_W_rows, axis=1)) / tf.size(W, out_type=tf.float32)
 
 
 def operator_norm(W):
     d1 = K.shape(W)[-2:]
     all_Ws = K.reshape(W, (-1, d1, d2))
     # ord 2 is operator norm: https://stackoverflow.com/questions/48275198/how-does-numpy-linalg-norm-ord-2-work
-    return K.sum(tf.norm(all_Ws, ord=2)) / tf.size(W)
+    return K.sum(tf.norm(all_Ws, ord=2)) / tf.size(W, out_type=tf.float32)
